fdb22e5c2d0f56f9d54e38a3ea5359c538bdff46
rameshpokharel21 <pokharelramesh@yahoo.com>
2025-09-19 20:47:16 -0400

    background EventDetails changed

--- src/components/EventDetails.jsx
+++ src/components/EventDetails.jsx
@@ -27,7 +27,7 @@ const EventDetails = () => {
       >
         {/* Gradient Accent */}
         <div
-          className="absolute inset-0 bg-gradient-to-r from-indigo-400 via-green-400 to-red-400
+          className="absolute inset-0 bg-gradient-to-r from-green-400 via-yellow-400 to-red-400
             opacity-20 rounded-xl pointer-events-none"
         ></div>
 
