aed60c912eb7dd570c3b8a36c6d8448249eedf9a
Ramesh.Pokharel <pokharelramesh@yahoo.com>
2025-09-22 08:44:28 -0400

    changing authcontext and app for starting page

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import getUser from "../services/api";
 
 export const AuthContext = createContext();
@@ -8,31 +8,30 @@ export const AuthProvider = ({ children }) => {
     isAuthenticated: false,
     user: null, //{username, roles: []}
     loading: true,
+    error: null,
   });
+
+  const fetchUser = useCallback(async() => {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), 15000);
+    try{
+      const res = await getUser();
+      setAuth({isAuthenticated: true, user: res.data, loading: false, error: null});
+    }catch{
+      setAuth({isAuthenticated: false, user: null, loading: false, error: "Unable to connect."});
+    }finally{
+      clearTimeout(timeout);
+    }
+  }, []);
 
   //check user authenticated when mounted
   useEffect(() => {
-
-    const controller = new AbortController();
-    const timeout = setTimeout(() => controller.abort(), 15000);
-
-    const fetchUser = async() => {
-      try{
-        const res = await getUser();
-        setAuth({isAuthenticated: true, user: res.data, loading: false});
-      }catch{
-        setAuth({isAuthenticated: false, user: null, loading: false});
-      }finally{
-        clearTimeout(timeout);
-      }
-    };
-
     fetchUser();
-    return () => controller.abort();//cleanup
-  }, []);
+    
+  }, [fetchUser]);
 
   return (
-    <AuthContext.Provider value={{ ...auth, setAuth }}>
+    <AuthContext.Provider value={{ ...auth, setAuth,fetchUser }}>
       {children}
     </AuthContext.Provider>
   );
