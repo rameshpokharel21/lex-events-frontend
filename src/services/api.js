90edf26af035ba048578dcadad3da3874f710c99
Ramesh.Pokharel <pokharelramesh@yahoo.com>
2025-09-22 22:46:42 -0400

    vercel.json with proxy now

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lex-events-backend.onrender.com/api",
+  baseURL: "/api",
   //import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
