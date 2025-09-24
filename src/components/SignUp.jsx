2aa0ade9f0539bbf40ee2fca0d05fc650338b78a
Ramesh.Pokharel <pokharelramesh@yahoo.com>
2025-09-24 18:08:16 -0400

    button centered fixed

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -149,9 +149,9 @@ const SignUp = () => {
         </button>
       )}
     </form>
-    <div flex justify-center mt-4>
+    <div className="flex flex-center mt-4">
       <NavLink to="/login"
-        className="w-full px-4 py-2 text-center text-white bg-blue-400 hover:bg-blue-500 rounded transition duration-300"
+        className="w-full max-w-xs px-4 py-2 text-center text-white bg-blue-400 hover:bg-blue-500 rounded transition duration-300"
         >
           Back to Login
         </NavLink>
