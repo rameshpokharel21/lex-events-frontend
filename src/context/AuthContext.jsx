6475110eff2456ae2156da9f77746780d0cde57b
rameshpokharel21 <pokharelramesh@yahoo.com>
2026-04-19 16:45:26 -0400

    React Query added changes on main.jsx and AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,46 +1,56 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext} from "react";
 import getUser from "../services/api";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({
-    isAuthenticated: false,
-    user: null, //{username, roles: []}
-    loading: false,
-    error: null,
+
+  const queryClient = useQueryClient();
+
+  const {data:user, isLoading} = useQuery({
+    queryKey: ["user"],
+    queryFn: async ({signal}) => {
+      try{
+        const response = await getUser(signal);
+        return {
+          ...response,
+          id: response.userId,
+        }
+      }catch(err){
+        console.log("X User fetch error: ", err);
+        console.log(err.response?.data);
+        console.log(err.message);
+        if(err.response?.status === 401){
+          return null;
+        }
+        throw err;
+      }
+    },
+    retry: (failureCount, error) => {
+      if(error.response?.status === 401){
+        return false;
+      }
+      return failureCount < 1;
+    },
+    staleTime: 5*60*1000,
+    refetchOnWindowFocus: true,
   });
 
-  const fetchUser = useCallback(async() => {
-    const controller = new AbortController();
-    const timeout = setTimeout(() => controller.abort(), 15000);
-    setAuth(prev => ({...prev, loading: true, error: null}));
-    
-    try{
-      const res = await getUser();
-      setAuth({isAuthenticated: true, user: res.data, loading: false, error: null});
-    }catch(err){
-      if(err.response?.status === 401){
-        setAuth({isAuthenticated: false, user: null, loading: false, error: null});
-      }else{
-        console.error("Error fetching user: ", err);
-        setAuth({isAuthenticated: false, user: null, loading: false, error: "Unable to connect."});
-      }
-      
-    }finally{
-      clearTimeout(timeout);
-    }
-  }, []);
+  const fetchUser = () => {
+    queryClient.invalidateQueries({queryKey: ["user"]})
+  }
 
-  //check user authenticated when mounted
-  // useEffect(() => {
-  //   fetchUser();
-    
-  // }, [fetchUser]);
+  const value = {
+    isAuthenticated: !!user,
+    user: user,
+    fetchUser,
+    loading: isLoading,
+  }
 
   return (
-    <AuthContext.Provider value={{ ...auth, setAuth, fetchUser }}>
+    <AuthContext value={value}>
       {children}
-    </AuthContext.Provider>
+    </AuthContext>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </AuthProvider>
+  </QueryClientProvider>
 );
