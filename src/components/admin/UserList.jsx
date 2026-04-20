73a0a0dd100e5c18a5073e9d49c448c340d9786c
rameshpokharel21 <pokharelramesh@yahoo.com>
2026-04-20 12:41:43 -0400

    UserList.jsx with react query

--- src/components/admin/UserList.jsx
+++ src/components/admin/UserList.jsx
@@ -1,50 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { deleteUser, fetchAllUsers } from "../../services/api";
 import Spinner from "../Spinner";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const UserList = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState("");
+  const queryClient = useQueryClient();
+
   const [showConfirm, setShowConfirm] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
-  const [deletingId, setDeletingId] = useState(null);
+ 
+  const {data: users = [], isLoading, error} = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchAllUsers,
+  });
 
+  const deleteMutation = useMutation({
+    mutationFn: userId => deleteUser(userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["users"]});
+    },
+    onSettled: () => {
+      setShowConfirm(false);
+      setSelectedUser(null);
+    },
+  });
   const confirmDelete = (user) => {
     setSelectedUser(user);
     setShowConfirm(true);
-  };
-
-  useEffect(() => {
-    const loadUsers = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetchAllUsers();
-        //console.log("from UserList", res);
-        setUsers(res);
-      } catch (err) {
-        setError("Failed to fetch users." + err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    loadUsers();
-  }, []);
-
-  const handleDelete = async () => {
-    if (!selectedUser) return;
-
-    try {
-      await deleteUser(selectedUser.userId);
-      setUsers((prev) => prev.filter((u) => u.userId !== selectedUser.userId));
-    } catch (err) {
-      console.error("Failed to detlete user: ", err);
-      setError("Failed to delete user." + err.message);
-    } finally {
-      setShowConfirm(false);
-      setSelectedUser(null);
-    }
   };
 
   if (isLoading) return <Spinner />;
@@ -53,7 +35,7 @@ const UserList = () => {
     return (
       <div className="p-4">
         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded">
-          {error}
+          Failed to fetch users. {error.message}
         </div>
       </div>
     );
@@ -75,14 +57,14 @@ const UserList = () => {
           </div>
           <button
             onClick={() => confirmDelete(user)}
-            disabled={deletingId === user.userId}
+            disabled={deleteMutation.isPending && selectedUser?.userId === user.userId}
             className={`px-3 py-1 rounded text-white ${
-              deletingId === user.userId
+              deleteMutation.isPending && selectedUser?.userId === user.userId
                 ? "bg-gray-400 cursor-not-allowed"
                 : "bg-red-500 hover:bg-red-600"
             }`}
           >
-            {deletingId === user.userId ? "Deleting..." : "Delete"}
+            {deleteMutation.isPending && selectedUser?.userId === user.userId ? "Deleting..." : "Delete"}
           </button>
         </div>
       ))}
@@ -101,14 +83,13 @@ const UserList = () => {
                 onClick={() => {
                   setShowConfirm(false);
                   setSelectedUser(null);
-                  setDeletingId(null);
                 }}
               >
                 Cancel
               </button>
               <button
                 className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded"
-                onClick={handleDelete}
+                onClick={() => deleteMutation.mutate(selectedUser.userId)}
               >
                 Confirm
               </button>
